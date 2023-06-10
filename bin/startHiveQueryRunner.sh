63254474bbefd5d8b8cf23bc9d057f2f052da273
Christian Zentgraf <kitgocz@gmail.com>
2023-06-10 01:50:13 -0400

    Use maven to start hive runner

--- bin/startHiveQueryRunner.sh
+++ bin/startHiveQueryRunner.sh
@@ -19,9 +19,9 @@ fi
 
 # Find the presto_server executable - it could be in 3 locations depending on the how the build was done.
 if [ -e "${PRESTO_TOP}/presto-native-execution/_build/debug/presto_cpp/main/presto_server" ]; then
-    PRESTO_SERVER_VAL=${PRESTO_TOP}/presto-native-execution/presto_cpp/main/presto_server
+    PRESTO_SERVER_VAL=${PRESTO_TOP}/presto-native-execution/_build/debug/presto_cpp/main/presto_server
 elif [ -e "${PRESTO_TOP}/presto-native-execution/_build/release/presto_cpp/main/presto_server" ]; then
-    PRESTO_SERVER_VAL=${PRESTO_TOP}/presto-native-execution/presto_cpp/main/presto_server
+    PRESTO_SERVER_VAL=${PRESTO_TOP}/presto-native-execution/_build/release/presto_cpp/main/presto_server
 elif [ -e "${PRESTO_TOP}/presto-native-execution/presto_cpp/main/presto_server" ]; then
     PRESTO_SERVER_VAL=${PRESTO_TOP}/presto-native-execution/presto_cpp/main/presto_server
 else
@@ -33,49 +33,5 @@ export PRESTO_SERVER=${PRESTO_SERVER_VAL}
 export DATA_DIR=${PRESTO_HOME}/data
 export WORKER_COUNT=0
 
-# Build the classpath
-JARS=`ls ${PRESTO_HOME}/lib`
-CLASSPATH="${PRESTO_TOP}/presto-native-execution/target/test-classes:\
-${PRESTO_TOP}/presto-hive/target/classes:\
-${PRESTO_TOP}/presto-hive/target/test-classes:\
-${PRESTO_TOP}/presto-hive-metastore/target/classes:\
-${PRESTO_TOP}/presto-tests/target/classes:\
-${PRESTO_TOP}/presto-tpch/target/classes:\
-${PRESTO_TOP}/presto-tpcds/target/classes:\
-${PRESTO_TOP}/presto-orc/target/classes:\
-${PRESTO_TOP}/presto-delta/target/classes:\
-${PRESTO_TOP}/presto-hive-common/target/classes:\
-${PRESTO_TOP}/presto-parquet/target/classes:\
-${PRESTO_TOP}/presto-cache/target/classes:\
-${PRESTO_TOP}/presto-rcfile/target/classes:\
-${HOME}/.m2/repository/io/airlift/tpch/tpch/0.10/tpch-0.10.jar:\
-${HOME}/.m2/repository/com/teradata/tpcds/tpcds/1.2/tpcds-1.2.jar:\
-${HOME}/.m2/repository/org/apache/thrift/libthrift/0.9.3/libthrift-0.9.3.jar:\
-${HOME}/.m2/repository/org/apache/hudi/hudi-presto-bundle/0.12.0/hudi-presto-bundle-0.12.0.jar:\
-${HOME}/.m2/repository/com/google/cloud/bigdataoss/gcs-connector/hadoop2-1.9.17/gcs-connector-hadoop2-1.9.17.jar:\
-${HOME}/.m2/repository/com/google/cloud/bigdataoss/gcsio/1.9.17/gcsio-1.9.17.jar:\
-${HOME}/.m2/repository/com/google/cloud/bigdataoss/util/1.9.17/util-1.9.17.jar:\
-${HOME}/.m2/repository/com/google/cloud/bigdataoss/util-hadoop/hadoop2-1.9.17/util-hadoop-hadoop2-1.9.17.jar:\
-${HOME}/.m2/repository/com/google/flogger/flogger/0.3.1/flogger-0.3.1.jar:\
-${HOME}/.m2/repository/com/google/flogger/google-extensions/0.3.1/google-extensions-0.3.1.jar:\
-${HOME}/.m2/repository/com/google/flogger/flogger-system-backend/0.3.1/flogger-system-backend-0.3.1.jar:\
-${HOME}/.m2/repository/com/github/luben/zstd-jni/1.5.2-2/zstd-jni-1.5.2-2.jar:\
-${HOME}/.m2/repository/com/facebook/presto/hive/hive-apache/3.0.0-8/hive-apache-3.0.0-8.jar:\
-${HOME}/.m2/repository/com/facebook/presto/hive/hive-apache-jdbc/0.13.1-5/hive-apache-jdbc-0.13.1-5.jar:\
-${HOME}/.m2/repository/com/facebook/presto/hadoop/hadoop-apache2/2.7.4-9/hadoop-apache2-2.7.4-9.jar:\
-${HOME}/.m2/repository/com/facebook/hive/hive-dwrf/0.8.5/hive-dwrf-0.8.5.jar:\
-${HOME}/.m2/repository/com/facebook/presto/orc/orc-protobuf/13/orc-protobuf-13.jar:\
-${HOME}/.m2/repository/com/amazonaws/aws-java-sdk-s3/1.11.697/aws-java-sdk-s3-1.11.697.jar:\
-${HOME}/.m2/repository/com/amazonaws/aws-java-sdk-core/1.11.697/aws-java-sdk-core-1.11.697.jar:\
-${HOME}/.m2/repository/org/testng/testng/7.5/testng-7.5.jar"
-
-# Use newline as separator (default is space)
-# load more classes from the presto-server
-set -f; IFS='
-'
-for filename in ${JARS}; do
-    CLASSPATH=${CLASSPATH}:${PRESTO_HOME}/lib/$filename
-done
-set +f; unset IFS
-
-java -ea -Xmx5G -XX:+ExitOnOutOfMemoryError -Duser.timezone=America/Bahia_Banderas -Dhive.security=legacy -DDATA_DIR="${DATA_DIR}" -classpath "${CLASSPATH}" com.facebook.presto.nativeworker.HiveExternalWorkerQueryRunner
+cd ${PRESTO_TOP}
+./mvnw exec:java -pl presto-native-execution -Dexec.mainClass="com.facebook.presto.nativeworker.HiveExternalWorkerQueryRunner" -DjvmArgs="-Xmx5G -XX:+ExitOnOutOfMemoryError" -Duser.timezone=America/Bahia_Banderas -Dhive.security=legacy -DDATA_DIR="${DATA_DIR}" -DPRESTO_SERVER="${PRESTO_SERVER}" -DWORKER_COUNT="${WORKER_COUNT}" -Dexec.classpathScope=test
